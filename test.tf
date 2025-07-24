bd89b9c7ad0f5f37bedf4b3d373b5cd75398589e
Kyle Butler <54778108+kyle9021@users.noreply.github.com>
2025-07-24 11:18:19 -0400

    Update test.tf

--- test.tf
+++ test.tf
@@ -52,33 +52,7 @@ resource "aws_ebs_volume" "web_host_storage" {
 
 
 
-resource "aws_security_group" "web-node" {
-  # security group is open to the world in SSH port
-  name        = "${local.resource_prefix.value}-sg"
-  description = "${local.resource_prefix.value} Security Group"
-  vpc_id      = aws_vpc.web_vpc.id
 
-  ingress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
-    cidr_blocks = [
-    "0.0.0.0/0"]
-  }
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-    cidr_blocks = [
-    "0.0.0.0/0"]
-  }
-  egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    cidr_blocks = [
-    "0.0.0.0/0"]
-  }
   depends_on = [aws_vpc.web_vpc]
   tags = {
     git_commit           = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
